3a4e7ed05fee8f7fd4ec3a82a10d39da473b6216
jblackhawks82 <@jblackhawks82>
2024-04-09 15:19:47 -0400

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ReactWvite/",
+  base: "/reactpro/",
   plugins: [react()],
   server: 3001,
 })
